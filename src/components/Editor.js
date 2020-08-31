05ffb2a6ed2fdc7313b4caf99087cb780990b785
sumedh <summm02@gmail.com>
2020-08-31 11:06:19 +0530

    Added slider and historical view in separate text area

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
 
 class Editor extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             textArray: [],
+            currentPoint: ""
         }
     }
 
     handleKeyDown(e) {
         if(isValidKey(e.key)) {
             if(this.state.textArray.length > 0) {
+                const current = this.state.textArray[this.state.textArray.length - 1] + e.key; 
                 this.setState({
-                    textArray: [...this.state.textArray, this.state.textArray[this.state.textArray.length - 1] + e.key]
+                    textArray: [...this.state.textArray, current],
+                    currentPoint: this.state.currentPoint
                 })
             }
             else {
@@ -24,9 +29,38 @@ class Editor extends React.Component {
 
     }
 
+    onSliderChange(value) {
+        this.setState({
+            textArray: this.state.textArray,
+            currentPoint: this.state.textArray[value]
+        });
+    }
+
     render() {
         return (
-            <textarea className="formControl" onKeyDown={this.handleKeyDown.bind(this)}></textarea>
+            <div className="main">
+                <textarea className="formControl editor" onKeyDown={this.handleKeyDown.bind(this)}></textarea>
+                <Slider className="slider"
+                        min={0}
+                        max={this.state.textArray.length}
+                        onChange={this.onSliderChange.bind(this)}
+                        railStyle={{
+                            height: 2
+                        }}
+                        handleStyle={{
+                            height: 28,
+                            width: 28,
+                            marginLeft: 0,
+                            marginTop: -14,
+                            backgroundColor: "red",
+                            border: 1
+                        }}
+                        trackStyle={{
+                            background: "none"
+                        }}
+                />
+                <textarea className="formControl editor" disabled="disabled" value={this.state.currentPoint}></textarea>
+            </div>
         )
     }
 }
